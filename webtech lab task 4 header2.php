9f1cd937d55f5127276283f0137ff5b8bce2b029
babuchittagong <103102812+babuchittagong@users.noreply.github.com>
2023-03-02 01:15:51 +0600

    lab task 4

---
+++ webtech lab task 4 header2.php
@@ -0,0 +1,19 @@
+<?php
+    echo '
+    
+
+    <fieldset >
+
+        <p align="left" ><b>
+            <a href="homepage2.php" style="font-size:22px" >Home</a>&nbsp;&nbsp;&nbsp;
+            <a href="myProfile.php" style="font-size:22px" >My Profile</a>&nbsp;&nbsp;&nbsp;
+            <a href="editProfile.php" style="font-size:22px" >Update Profile</a>&nbsp;&nbsp;&nbsp;
+            <a href="orderFood.php" style="font-size:22px" >Food Order</a>&nbsp;&nbsp;&nbsp;
+            <a href="review.php" style="font-size:22px" >Review</a>&nbsp;&nbsp;&nbsp;
+            <a href="Logout.php" style="font-size:22px" >Log Out</a>
+        </p> 
+
+    </fieldset>
+    ';
+
+?>
---
+++ webtech lab task 4 home page 1.php
@@ -0,0 +1,52 @@
+<?php 
+
+session_start();
+
+
+if(isset($_SESSION['username'])){
+
+
+
+}
+else{
+
+    header('location: login.php');
+}
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+
+
+ <?php include('header2.php')?>
+<body>
+
+
+    <fieldset>
+
+        <p><br></p><p><br></p>
+        <h1 align="center" style= "color: #394393;">Hello, <?php echo $_SESSION['username'] ?> </span></h1>
+        <h1 align="center" style= "color: #394393;">Welcome</h1> 
+        <h1 align="center" style= "color: #394393;">To</h1>
+        <h1 align="center" style= "color: #394393;">Restaurant Management System</h1>
+
+
+    
+    <p><br></p><p><br></p>
+    </fieldset>
+</body>
+
+<?php include('footer.php')?>
+
+</html>
